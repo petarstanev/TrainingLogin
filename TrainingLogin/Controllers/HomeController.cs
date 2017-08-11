7cbdf2a4f0d66e48a3b9e1305b1a8575fc772fb7
Petar Stanev <petar.stanev@topcashback.co.uk>
2017-08-11 15:16:32 +0100

    Init. Added really simple login.

--- TrainingLogin/Controllers/HomeController.cs
+++ TrainingLogin/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrainingLogin.Models;
 
 namespace TrainingLogin.Controllers
 {
@@ -25,6 +26,52 @@ namespace TrainingLogin.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Test()
+        {
+            ViewBag.Message = "Test";
+
+            return View();
+        }
+
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Login";
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (user.Password == "123456")
+                {
+                    Session["UserId"] = 1010;
+                    return RedirectToAction("UserDashBoard");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult UserDashBoard()
+        {
+            if (Session["UserId"] != null)
+            {
+                User a = new User();
+                
+                a.Id = Int32.Parse(Session["UserId"].ToString());
+
+                return View(a);
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
         }
     }
 }
---
+++ TrainingLogin/Models/GroupOfUsers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrainingLogin.Models
+{
+    public class GroupOfUsers
+    {
+        public List<User> Users { get; set; }
+        public GroupOfUsers()
+        {
+            Users = new List<User>();
+        }
+    }
+}
